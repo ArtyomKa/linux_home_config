5e907c37c9e0e6da4f824eb8eb4a9f0093f15cdf
Artyom Katsap <artyom.katsap@amplifai.co>
2017-11-21 10:05:03 +0200

    added fonts installation

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 
 cur_dir=$(pwd)
+now=$(date +"%m_%d_%Y")
+
 echo $cur_dir
 cd $HOME
 
@@ -9,8 +11,25 @@ ln -sf ${cur_dir}/vim ${HOME}/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
-ln -sf ${cur_dir}/.bash_aliases $HOME/
-ln -sf ${cur_dir}/.pythonrc $HOME/
-ln -sf ${cur_dir}/.bashrc $HOME/
-ln -sf ${cur_dir}/.vimrc $HOME/
+files=(.bash_aliases .pythonrc  .bashrc .vimrc)
+
+#create symbolic links to the files:
+
+for f in "${files[@]}"
+do
+    mv $HOME/${f} $HOME/${f}_backup_$now
+    ln -sf ${cur_dir}/${f} $HOME/
+done
+
+
+#install poweline fonts for vim:
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
 
